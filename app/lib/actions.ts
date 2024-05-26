45540a57e7cd6e356c954e5961cccad96e46ca40
dododcdc <dcdc3166@foxmail.com>
2024-05-26 14:07:20 +0800

    cpt14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,6 +20,7 @@ const FormSchema = z.object({
   });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const EditInvoice = FormSchema.omit({ id: true, date: true });
 
 export type State = {
   errors?: {
@@ -72,14 +73,29 @@ export async function createInvoice(prevState: State,formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State,formData: FormData) {
+    // const { customerId, amount, status } = UpdateInvoice.parse({
+    //   customerId: formData.get('customerId'),
+    //   amount: formData.get('amount'),
+    //   status: formData.get('status'),
+    // });
+
+    const validatedFields = EditInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
    
-    const amountInCents = amount * 100;
+    const { customerId, amount, status } = validatedFields.data
+    const amountInCents = amount * 100 ;
+    // const amountInCents = amount * 100;
 
     try {
       await sql`
